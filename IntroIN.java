53554e66ec8e456781ca63388245f4adfa28cf94
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2020-05-30 22:31:32 +0530

    Create IntroIN.java

---
+++ IntroIN.java
@@ -0,0 +1,19 @@
+A Java program is structured in a specific and particular manner. 
+Java is a language and therefore has grammar similar to a spoken language like English.
+It is Simple & Portable: Memory Management using Pointers is not allowed.
+It is Object-Oriented.
+It is Independent of the host platform means it compiles differently for different Operating System.
+It is Secured & Dynamic: Designed to execute code from remote sources securely.
+It contains language facilities and libraries for Networking.
+Java gives High Performance: With the use of JIT (Just-In-Time) compilers.
+Java achieves high performance through the use of byte-code that can be easily translated into native machine code.
+It is Robust: Java has its own strong memory management system. This helps to eliminate errors as it checks the code during compile and runtime.
+Java is Multithreaded: It supports multiple executions of threads (i.e., lightweight processes), including a set of synchronization primitives.
+
+
+//Bad Practices in Java
+Use of Pointers
+Operator overloading
+Multiple inheritance
+Friend classes (access another object’s private members)
+Restrictions of explicit type casting (related to memory management)
