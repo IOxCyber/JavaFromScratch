99be6673862ad3252d040cf2bf348824a234ea49
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2020-05-30 22:51:01 +0530

    Create HelloWorldinDepthIN.java

---
+++ HelloWorldinDepthIN.java
@@ -0,0 +1,10 @@
+//Java is an object-oriented programming language. 
+//Therefore, every line that needs to be executed should be inside a class. This line declares a class HelloWorld.
+
+   class HelloWorld {//class HelloWorld is the class declaration.
+   
+   public static void main(String args[]) { //main is the point from where all the Java programs start its execution. 
+   
+        System.out.println("Hello World!");//Gives processor the command to output the literal specified in the double quotes ("") and adds a new line to the console. In this case, Hello World! is printed.
+      //System.out.print() prints the content and does not add the new line.
+   }//A block of code is defined with the { } tokens. { signifies the start of a block of code, ​and } signifies the end.
