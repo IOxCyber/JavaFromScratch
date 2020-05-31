0acc8b553368b927e526a38443998e2b836ba6a2
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2020-05-31 16:10:24 +0530

    Create VariablesIN.java

---
+++ VariablesIN.java
@@ -0,0 +1,14 @@
+Variables are just like containers which hold the values while the program is executed.
+A variable is a storage location paired with an associated symbolic name (an identifier), 
+which contains some known or unknown quantity of information referred to as a value.
+Variables in Java are strongly typed; thus they all must have a data type declared with their identifier.
+Variables, once declared in the program, are allocated space in memory. 
+Memory itself is organized as a long collection of one byte after another. 
+These bytes are sequentially numbered, with the number being referred to as a memory address for the corresponding location.
+It is customary to use hexadecimal numbers to show the addresses of memory locations.
+
+
+
+
+
+
