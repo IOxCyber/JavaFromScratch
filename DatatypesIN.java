82bbe268f0168fc7b00010b4e757bae7b19e3ed1
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2020-06-02 22:10:01 +0530

    Create DatatypesIN.java

---
+++ DatatypesIN.java
@@ -0,0 +1,43 @@
+public class Type_Floats {
+    public static void main(String[] args) {
+    
+// A short type is a 16 bit signed integer
+short age1; // Stores from +32,767 to -32,768	
+
+// An int type is a 32 bit signed integer
+int age2; // Stores from +2,147,483,647 to -2,147,483,648
+
+// A long type is a 64 bit signed integer
+long age3; // Stores from +9,223,372,036,854,775,807 to -9,223,372,036,854,775,808
+
+// Assigning a float to a radius of a circle
+        float radius = 8.56f;
+        System.out.println(radius);
+        double radius = 0.00000000000863872078;
+        System.out.println(radius)
+
+//A char is a 16-bit data type that contains either a single digit or a character. 
+//Each character is represented by a unique number according to a standard code like Unicode. 
+        char letter = 'A';
+        System.out.println(letter);
+
+        char number_as_text = '6';
+        System.out.println(number_as_text);
+
+        char number = 65;
+        System.out.println(number);
+        
+        //Boolean Type 
+        boolean part_time = false;
+        System.out.println(part_time);
+        
+          String line;
+     System.out.println(line);
+     
+     //Data Type	Size in bytes
+Integer	4 bytes
+Float	4 bytes
+Double	8 bytes
+Character	2 bytes
+Boolean	1 byte
+     }
