9d4f3f6c83fa28c202316a79b399f5ce72e73f49
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2020-06-03 23:20:49 +0530

    Update TheMathFunction.java

--- TheMathFunction.java
+++ TheMathFunction.java
@@ -4,10 +4,10 @@ everything from the java.lang package is automatically imported and available, s
 */
 public class exponent {
     public static void main(String[] args) {
-        System.out.println("2 raised to the power 3 is " + Math.pow(2, 3));
-        System.out.println("Exponent squared is " + Math.exp(2));
-        System.out.println("The square root of 16 is " + Math.sqrt(16));
-        System.out.println("The cube root of 27 is " + Math.cbrt(27));
+        System.out.println("2 raised to the power 3 is " + Math.pow(2, 3)); //power
+        System.out.println("Exponent squared is " + Math.exp(2)); //
+        System.out.println("The square root of 16 is " + Math.sqrt(16)); //square root
+        System.out.println("The cube root of 27 is " + Math.cbrt(27)); //cube root
         
         //Log
         System.out.println("log of 2 is " + Math.log(2));
