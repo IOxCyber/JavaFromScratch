5fa0b519cf69a1c25a61cb5932833064313d78ff
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2020-06-03 23:09:47 +0530

    Create logicalExpresionIN.java

---
+++ logicalExpresionIN.java
@@ -0,0 +1,47 @@
+class log_op {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 10;
+
+        System.out.println("x is equal to: " + x);
+        System.out.println("y is equal to: " + y);
+
+        System.out.println("x is greater than y");
+        System.out.println(x > y);
+
+        System.out.println("x is less than y");
+        System.out.println(x < y);
+
+        System.out.println("x is greater than or equal to y");
+        System.out.println(x >= y);
+
+        System.out.println("y is less than or equal to y");
+        System.out.println(y <= y);
+
+        System.out.println("x is equal to y");
+        System.out.println(x == y);
+
+        System.out.println("x is not equal to y");
+        System.out.println(x != y);
+        
+        //Boolean Expresion
+        boolean x = true;
+        boolean y = false;
+
+        System.out.println("Value of x: " + x);
+        System.out.println("Value of y: " + y);
+
+        System.out.println("Boolean NOT of x");
+        System.out.println(!x);
+
+        System.out.println("Boolean AND of x and y");
+        System.out.println(x && y);
+
+        System.out.println("Boolean OR of x and y");
+        System.out.println(x || y);
+
+        System.out.println("Boolean exclusive XOR of x and y");
+        System.out.println(x ^ y);
+    }
+        
+    }
