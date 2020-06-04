0e99b03b60ce794cd7ada89d2c6e48d44669125e
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2020-06-04 13:16:02 +0530

    Create ChapterQuizIN.java

---
+++ ChapterQuizIN.java
@@ -0,0 +1,14 @@
+1. What is the answer of the following expression?
+(2+3)-10 * 5;
+ans. (-45)
+
+2. Which of the following methods does NOT take one input?
+ams. Math.pow(digit,RaiseToPower)
+
+3.What is the result of the following logical expression?
+( 5>= Math.pow(2,3))
+ans. false //8
+
+4.What is the result of the following expression?
+((true && true) || false) ^ false
+ans. true
