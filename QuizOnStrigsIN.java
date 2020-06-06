ba7be988d1572dd8875afb9faa9130e81d633b4b
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2020-06-06 20:17:37 +0530

    Create QuizOnStrigsIN.java

---
+++ QuizOnStrigsIN.java
@@ -0,0 +1,21 @@
+1. Which special character is used to add a new line in Strings?
+ans. \n
+
+2.What will be the output of the following code?
+String name="   John Doe   ";
+name.trim();
+System.out.println(name);
+Note: The spaces in the name are signified by - in the answers
+ans. —John Doe—
+
+3.What is the output of the following code?
+String text="Hi my name is";
+String name="John";
+System.out.println(text+name);
+ans. Hi my name isJohn
+
+4.
+What is the substring extracted by this code?
+String text="Please bring my coffee";
+System.out.println(text.substring(6,11));
+ans. -brin
