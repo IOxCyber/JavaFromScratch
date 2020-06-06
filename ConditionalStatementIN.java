1e6b222612c81d8918f51cc277da638c667cef31
Saransh_chauhan <40174034+SaranshChauhan@users.noreply.github.com>
2020-06-06 20:33:20 +0530

    Create ConditionalStatementIN.java

---
+++ ConditionalStatementIN.java
@@ -0,0 +1,45 @@
+//if Statement
+
+class conditional {
+ public static void main(String[] args) {
+  int x = 10;
+
+  if (x > 4) {
+   System.out.println("x is greater than 4");
+  }
+ }
+}
+
+
+//if-else Statement
+class conditional {
+    public static void main(String[] args) {
+        int x = 1;
+        
+        if (x > 4) {
+            System.out.println("x is greater than 4");
+        } 
+        else {
+            System.out.println("x is less than 4");
+        }
+    }
+}
+
+
+//If-ELse if
+
+class conditional {
+    public static void main(String[] args) {
+        int x = 4;
+
+        if (x > 4) {
+            System.out.println("x is greater than 4");
+        }
+        else if (x == 4) {
+            System.out.println("x is equal to 4");
+        } 
+        else {
+            System.out.println("x is less than 4");
+        }
+    }
+}
